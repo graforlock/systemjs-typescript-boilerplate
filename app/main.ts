4ed60d4080ac3e1d0fff6a4256ab6403ab17ca80
Maciej Sitko <maciej@neatcube.co.uk>
2016-05-08 20:44:01 +0100

    ES6 Shim for Promises

--- app/main.ts
+++ app/main.ts
@@ -1,3 +1,5 @@
+import rxjs from 'rxjs';
+
 window.addEventListener('click', () => {
    System.import('./app/imp').then(({button}) => {
         console.log(button);
--- config.js
+++ config.js
@@ -12,29 +12,57 @@ System.config({
     "github:*": "jspm_packages/github/*",
     "npm:*": "jspm_packages/npm/*"
   },
+
   packages: {
     "/": {
       "defaultExtension": "js",
-      map: {
-        ts: 'github:frankwallis/plugin-typescript@4.0.9'
+      "map": {
+        "ts": "github:frankwallis/plugin-typescript@4.0.9"
       }
     }
   },
 
   map: {
+    "rxjs": "./node_modules/rxjs/bundles/Rx.umd.js",
     "ts": "github:frankwallis/plugin-typescript@4.0.9",
+    "ts-runtime": "npm:babel-runtime@5.8.38",
     "typescript": "npm:typescript@1.8.10",
     "github:frankwallis/plugin-typescript@4.0.9": {
       "typescript": "npm:typescript@1.8.10"
     },
+    "github:jspm/nodelibs-assert@0.1.0": {
+      "assert": "npm:assert@1.3.0"
+    },
     "github:jspm/nodelibs-os@0.1.0": {
       "os-browserify": "npm:os-browserify@0.1.2"
+    },
+    "github:jspm/nodelibs-process@0.1.2": {
+      "process": "npm:process@0.11.3"
+    },
+    "github:jspm/nodelibs-util@0.1.0": {
+      "util": "npm:util@0.10.3"
+    },
+    "npm:assert@1.3.0": {
+      "util": "npm:util@0.10.3"
+    },
+    "npm:babel-runtime@5.8.38": {
+      "process": "github:jspm/nodelibs-process@0.1.2"
+    },
+    "npm:inherits@2.0.1": {
+      "util": "github:jspm/nodelibs-util@0.1.0"
     },
     "npm:os-browserify@0.1.2": {
       "os": "github:jspm/nodelibs-os@0.1.0"
     },
+    "npm:process@0.11.3": {
+      "assert": "github:jspm/nodelibs-assert@0.1.0"
+    },
     "npm:typescript@1.8.10": {
       "os": "github:jspm/nodelibs-os@0.1.0"
+    },
+    "npm:util@0.10.3": {
+      "inherits": "npm:inherits@2.0.1",
+      "process": "github:jspm/nodelibs-process@0.1.2"
     }
   }
 });
